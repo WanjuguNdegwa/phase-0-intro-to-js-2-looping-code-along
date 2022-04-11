6570b8271bb2cb31e8d0d93f603e6dd20e203b49
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-04-11 06:03:56 +0300

    commit tree 9ca7bcff92893515433630336b309dad0237f6f8

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+function writeCards(arr, text) {
+    return arr.map((item) => `Thank you, ${item}, for the wonderful ${text} gift!`)    
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
